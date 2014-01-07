bcbadd2585ebc435426eb8b1d2d6c1a9fdf912da
Paul Pound <ppound@upei.ca>
2014-01-07 10:35:48 -0400

    changes for newspaper toolbar allows toolbar to show in full screen view

--- theme/islandora-newspaper-page-controls.tpl.php
+++ theme/islandora-newspaper-page-controls.tpl.php
@@ -4,6 +4,8 @@
  * Displays the newspaper page controls.
  */
 ?>
-<div class="islandora-newspaper-controls">
-  <?php print theme('item_list', array('items' => $controls, 'attributes' => array('class' => array('items', 'inline')))); ?>
+<div class="islandora-openseadragon-controls clearfix" id="islandora-openseadragon-controls">
+  <div class="islandora-newspaper-controls-toolbar clearfix" id="islandora-newspaper-controls-toolbar">
+    <?php print theme('item_list', array('items' => $controls, 'attributes' => array('class' => array('items', 'inline')))); ?>
+  </div>
 </div>
